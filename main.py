3241d3ede518c9b5a5114e5f01e1cb828df40ee5
Dorantes98 <dominickadorantes98@gmail.com>
2025-02-25 16:42:08 -0800

    created functions get_book_text and count_words

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+    
+frankenstein = get_book_text("books/frankenstein.txt")
+
+def count_words(text):
+    return len(text.split())
+
+word_count = count_words(frankenstein)
+
+def main():
+    # print(frankenstein)
+    print(f"{word_count} words found in the document")
+
+if __name__ == '__main__':
+    main()
